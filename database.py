307dcdace0e5f7b7a3276f8258f262186c1a6db0
CamilleBreuil <camille.breuil.pro@gmail.com>
2019-11-25 17:42:35 -0500

    Ajout de sib dans database.py et modification de test.py

--- database.py
+++ database.py
@@ -1,57 +1,51 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+This is the class Database
 
-This is a temporary script file.
+Created on Mon Nov 23 10:06:37 2019
+
+@author: camille
 """
 
 from collections import defaultdict
 
 class Database(object):
-    """    This is a class for 
+    """    
+    This is a class for data structure relevant for food classification.
       
     Attributes: 
-        graph (list): 
-        img (dict): 
-        sib (dict)
+        graph (list): A list of tuples (child_node,parent_node) defining a graph stucture. 
+        extract (dict): A dictionary where keys are image names and values node IDs (strings).
+        sib (dict): A dictionary where keys are image names with node IDS and values numbers of siblings of the node in the graph 
+        at the moment t when the extract is added.
     """
   
 
-
-    def __init__(self,graph=("core", None)):
+    def __init__(self, graph=("core", None)):
         """    
         The constructor for Database Class.
+        
+        Parameters:
+            graph (list): A list of tuples (child_node,parent_node) defining a graph stucture. 
       
-        Attributes: 
-            graph (list): 
-            img (dict): 
-            sib (dict) 
         """
 
         # Create a reference abstract category ("core") if not already present to instantiate the graph
         if graph[0] != ("core", None):
-            graph.insert(0,("core", None))
+            graph.insert(0, ("core", None))
      
         # Instantiate the attributes 
-        self.graph=graph
-        self.extract={}
-        #self.extract_status= {} 
-        self.sib={}
-        
-        # Compute and update the number of siblings of each node in the initial graph 
-        #for item in self.graph[1:]:
-        #    self.sib[item[0]] = self.count_sib(item[0])
+        self.graph = graph
+        self.extract = {}
+        self.sib = {}
      
-     
-
-
 
     def count_sib(self, node_to_count):
         """    
-        The function to count the number of siblings for one node (or list of nodes)
+        The function to count the number of siblings for one node.
       
         Parameters: 
-            node_to_count (str) 
+            node_to_count (str): node ID 
             
         Returns:
             nb_sib (int) : number of siblings of the node 
@@ -61,19 +55,18 @@ class Database(object):
         # Find the parent node of node_to_count 
         parent = [item[1] for item in self.graph[1:] if item[0]==node_to_count][0]
         
-        # Compute the number of sibling nodes(=having the same parent)
+        # Compute the number of sibling (=having the same parent) nodes
         nb_sib = len([item[0] for item in self.graph[1:] if item[1]==parent])-1
    
         return nb_sib
     
               
-    
-    def add_nodes(self,nodes_to_add):
+    def add_nodes(self, nodes_to_add):
         """ 
         Add one or several nodes and edit the graph
         
         Parameters:
-        node_to_add  (list of tuples) :
+            node_to_add (list) : A list of tuples (child_node,parent_node) to be added to the existing graph
             
         """
         
@@ -89,24 +82,19 @@ class Database(object):
                     self.graph.insert(max([i for i, item in enumerate(self.graph) if new_parent in item])+1,(new_child, new_parent))
                     print("Node " + new_child + " added")
                     
-        
-        #self.update_extract_status()  
-        
-        # Update sib
-       # for item in self.graph[1:]:
-        #    self.sib[item[0]] = self.count_sib(item[0])
-            
-           
                     
-    def add_extract(self,new_extract):
+    def add_extract(self, new_extract):
         """ 
-        Add an extract to img attribute of the Database
+        Add an extract to extract attribute of the Database object.
         
-        Parameters
-        extract : dictionnay key=image, value=node
+        Parameters:
+            new_extract (dict): A dictionary where keys are image names and values node IDs (strings) to be added to the existing extract
         """
+        
+        # Update the extract attribute considering the new extract
         self.extract.update(new_extract)
         
+        # Count the number of siblings of each node of each image from the new_extract when added and save these values in sib attribute
         for key in new_extract:
             for value in new_extract[key]:
                 if value in [item[0] for item in self.graph]:
@@ -114,75 +102,22 @@ class Database(object):
                 else: 
                     self.sib[str(key+','+value)]=0
                 
-            
-    
-#    def update_extract_status(self): 
-#        
-#        for key in self.extract_status.keys():
-#            if  self.extract_status[key]=='':
-#            
-#                # Initialize a dict for status and temp_status to temporary save the status of each label of an image 
-#                #status = defaultdict(list)
-#                temp_status = defaultdict(list)
-#
-#            
-#                # Is there any label for the image?
-#                if self.extract[key] == []:
-#                    self.extract_status[key] = "valid"
-#                    
-#                else:
-#                    # Is the label of the image a node of the graph?
-#                    for node in self.extract[key]:
-#                        
-#                        if node not in [item[0] for item in self.graph]:
-#                            temp_status[key].append("invalid")
-#                            
-#                        else:
-#                            # Does the node have new siblings since the extract was added?
-#                            if node in self.sib.keys():
-#                                new_nb_sib = self.count_sib(node) 
-#                                
-#                                if new_nb_sib > self.sib[node]:
-#                                    temp_status[key].append("coverage_staged")
-#                                    
-#                                # Does the node have any child node? 
-#                                else:
-#                                    if node in [item[1] for item in self.graph]:
-#                                        temp_status[key].append("granularity_staged")
-#            
-#                                    else: 
-#                                        temp_status[key].append("valid")
-#                    
-#                       
-#                                            
-#                        # Returns one status for each image
-#                        if "invalid" in temp_status[key]:
-#                            self.extract_status[key] = "invalid"
-#                        elif "coverage_staged" in temp_status[key]:
-#                            self.extract_status[key] = "coverage_staged"
-#                        elif "granularity_staged" in temp_status[key]:
-#                            self.extract_status[key] = "granularity_staged"
-#                        else: self.extract_status[key]= "valid"
-#    
-#        
-        
-        
-           
+          
     def get_extract_status(self):
         """ 
-        Return the status of 
-        
-        Parameters:
-        extract : dictionnay key=image, value=node
-        
+        Return the status of the extract.
+                
         Returns:
+            status (dict): a dictionary where keys are names of images in the extract and values the status of each image. 
         """
-        # Initialize a dict for status and temp_status to temporary save the status of each label of an image 
+        # Initialize an empty dict for status 
         status = defaultdict(list)
-        temp_status = defaultdict(list)
+
         
-        # For each image of the img dict, for each label, verify the status.
+        # For each image of the extract attribute, for each label of the image, verify the status.
         for key in self.extract:
+            # Initialize a temp_status, a dict to temporary save a status of each label of an image
+            temp_status = defaultdict(list) 
             
             # Is there any label for the image?
             if self.extract[key] == []:
@@ -197,14 +132,13 @@ class Database(object):
                         
                     else:
                         # Does the node have new siblings since the extract was added?
-
                         new_nb_sib = self.count_sib(node) 
                             
                         if new_nb_sib > self.sib[str(key+','+node)]:
                             temp_status[key].append("coverage_staged")
-                                
-                            # Does the node have any child node? 
+                                                           
                         else:
+                            # Does the node have any child node? 
                             if node in [item[1] for item in self.graph]:
                                 temp_status[key].append("granularity_staged")
         
@@ -222,10 +156,6 @@ class Database(object):
                         status[key] = "granularity_staged"
                     else: status[key]= "valid"
             
-        #Update 
-        #for item in self.graph[1:]:
-        #    self.bro[item[0]] = self.count_bro(item[0])
-        #status = dict(zip(self.status_extract.keys(),[self.status_extract.values()[key][1] for key in self.status_extract.keys()]))
         print(status)
         
         return(status)
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+This is the code to test the class Database
+
 Created on Mon Nov 25 14:05:39 2019
 
 @author: camille
@@ -10,55 +12,88 @@ import json
 
 from database import Database
 
-build=[("core", None), ("A", "core"), ("B", "core"), ("C", "core"), ("C1", "C")]
-extract={"img001": ["A", "B"], "img002": ["A", "C1"]}
-edits=[("B1", "B"), ("C2", "C")]
-extract2={"img003": ["A", "C1"], "img004": ["B"]}
-edits2 =[("C3", "C")]
 
+
+## Test on a basic dataset
+
+build = [("core", None), ("A", "core"), ("B", "core"), ("C", "core"), ("C1", "C")]
+
+extract = {"img001": ["A", "B"], "img002": ["A", "C1"]}
+
+edits = [("B1", "B"), ("C2", "C")]
+
+extract2 = {"img003": ["A", "C1"], "img004": ["B"]}
+
+edits2 =[ ("C3", "C")]
+
+# Create a Database initialized with graph = build to test the constructor
 db=Database(build)
 
+# Add an extract to test the add_extract function
 db.add_extract(extract)
 
+# Get the extract status to test the get_extract_status function
 db.get_extract_status()
 
+# Add nodes to test the add_nodes function
 db.add_nodes(edits)
 
+# Verify the new extract status
 db.get_extract_status()
 
+# Add an other extract
 db.add_extract(extract2)
 
+# Verify the new extract status
 db.get_extract_status()
 
+# Add other nodes
 db.add_nodes(edits2)
 
+# Verify the new extract status
 db.get_extract_status()
 
 
 
 
-### Import data 
+### The on the provided dataset 
 
+# Import the data 
 with open('./data/graph_build.json', 'rb') as f:
- graph_build = json.load(f)
+    graph_build = json.load(f)
  
 with open('./data/img_extract.json', 'rb') as f:
- img_extract = json.load(f)
+    img_extract = json.load(f)
  
 with open('./data/graph_edits.json', 'rb') as f:
- graph_edits = json.load(f)
+    graph_edits = json.load(f)
  
 with open('./data/expected_status.json', 'rb') as f:
- expected_status = json.load(f)
+    expected_status = json.load(f)
 
- db2 = Database(graph_build)
- db2.add_extract(img_extract)
- db2.get_extract_status()
- db2.add_nodes(graph_edits)
- get_status2={}
- get_status2 = db2.get_extract_status()
+# Create a Database initialized with graph = graph_build 
+db2 = Database(graph_build)
  
- if get_status2 == expected_status: 
-    print ("YOUPI ÇA MARCHE !!!!") 
- else: 
-    print("oh non" )
+# Add the extract img_extract
+db2.add_extract(img_extract)
+ 
+# Check the extract status
+db2.get_extract_status()
+ 
+# Add the nodes graph_edits
+db2.add_nodes(graph_edits)
+ 
+# Get the final 
+final_status = db2.get_extract_status()
+ 
+#Compare the final status with expected status 
+if final_status == expected_status: 
+    print ("YOUPIII!!!!") 
+    
+else: 
+    print("oh no..." )
+    
+    for key in expected_status.keys():
+        if final_status[key] != expected_status[key]:
+            print("Image "+ key + " get status "+ final_status[key] + "while expected " +expected_status[key])
+            
