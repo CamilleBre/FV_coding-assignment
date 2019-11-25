454aeacbc6df2ddc04fa6319fd49d085aedfbb41
CamilleBreuil <camille.breuil.pro@gmail.com>
2019-11-25 16:12:52 -0500

    database.py

--- database.py
+++ database.py
@@ -8,222 +8,212 @@ This is a temporary script file.
 from collections import defaultdict
 
 class Database(object):
+    """    This is a class for 
+      
+    Attributes: 
+        graph (list): 
+        img (dict): 
+        sib (dict)
+    """
+  
 
-    def __init__(self,graph=("core", None),img={}):
 
-     #Créer une liste de tuples 
-     if graph[0] != ("core", None):
-         graph.insert(0,("core", None))
+    def __init__(self,graph=("core", None)):
+        """    
+        The constructor for Database Class.
+      
+        Attributes: 
+            graph (list): 
+            img (dict): 
+            sib (dict) 
+        """
+
+        # Create a reference abstract category ("core") if not already present to instantiate the graph
+        if graph[0] != ("core", None):
+            graph.insert(0,("core", None))
      
-    
-     self.graph=graph
-     self.img=img
-     
-     self.bro={}
-     #self.bro.update([{item[0],self.count_bro(item[0])} for item in self.graph[1:]])
-     #self.bro([item[0] for item in self.graph[1:]]) = self.count_bro(item[0])
-     for item in self.graph[1:]:
-         self.bro[item[0]] = self.count_bro(item[0])
-     
-     
-     
+        # Instantiate the attributes 
+        self.graph=graph
+        self.extract={}
+        #self.extract_status= {} 
+        self.sib={}
         
-       
-         
-     #for item in self.graph[1:] :
-     #    self.bro[item[0]] = sum([x[1]==item[1] for x in self.graph[1:]])-1
-         
+        # Compute and update the number of siblings of each node in the initial graph 
+        #for item in self.graph[1:]:
+        #    self.sib[item[0]] = self.count_sib(item[0])
+     
      
 
 
-    def count_bro(self, node_to_count):
+
+    def count_sib(self, node_to_count):
+        """    
+        The function to count the number of siblings for one node (or list of nodes)
+      
+        Parameters: 
+            node_to_count (str) 
+            
+        Returns:
+            nb_sib (int) : number of siblings of the node 
+            
+        """
+        
+        # Find the parent node of node_to_count 
         parent = [item[1] for item in self.graph[1:] if item[0]==node_to_count][0]
-        nb_bro = len([item[0] for item in self.graph[1:] if item[1]==parent])-1
-   
-        return nb_bro
-    
-                
-    def add_nodes(self,nodes_to_add):
-#    """ Add one or several nodes and edit the graph
-#    
-#    Parameters
-#    ----------
-#    node_to_add : list of tuples 
-#    """
         
-    #### RAJOUTER DES CONDITIONS POUR NONE 
-       
+        # Compute the number of sibling nodes(=having the same parent)
+        nb_sib = len([item[0] for item in self.graph[1:] if item[1]==parent])-1
+   
+        return nb_sib
+    
+              
+    
+    def add_nodes(self,nodes_to_add):
+        """ 
+        Add one or several nodes and edit the graph
+        
+        Parameters:
+        node_to_add  (list of tuples) :
+            
+        """
+        
         for new_child, new_parent in [item for item in nodes_to_add if item != ("core", None)]:  
             if len([item for item in self.graph if new_child in item])>0:
-                print(" CE NOEUD EXISTE DEJA!")
+                print("Node " + new_child + " already exists")
+                
             else:
                 if len([item for item in self.graph if new_parent in item])==0:
-                    print("Noeud parent inexistant!")
+                    print("Error:" + new_parent + " can't be a parent node because it doesn't exist")
+                    
                 else:
                     self.graph.insert(max([i for i, item in enumerate(self.graph) if new_parent in item])+1,(new_child, new_parent))
-                    print("noeud", new_child, "ajouté!")
+                    print("Node " + new_child + " added")
                     
         
+        #self.update_extract_status()  
+        
+        # Update sib
+       # for item in self.graph[1:]:
+        #    self.sib[item[0]] = self.count_sib(item[0])
+            
+           
                     
-                    
-    def add_extract(self,extract):
-        """ Add an extract as a dict 
+    def add_extract(self,new_extract):
+        """ 
+        Add an extract to img attribute of the Database
         
         Parameters
-        ----------
-        extrat : dictionnay key=image, value=node
+        extract : dictionnay key=image, value=node
         """
-        self.img.update(extract)
+        self.extract.update(new_extract)
         
+        for key in new_extract:
+            for value in new_extract[key]:
+                if value in [item[0] for item in self.graph]:
+                    self.sib[str(key+','+value)]=self.count_sib(value)
+                else: 
+                    self.sib[str(key+','+value)]=0
+                
+            
     
-#    def get_extract_status(self):
-#        status={}
-#        for key in self.img:
-#            nkey = len([item for item in self.graph if item[1] == self.img[key][0]])
+#    def update_extract_status(self): 
+#        
+#        for key in self.extract_status.keys():
+#            if  self.extract_status[key]=='':
 #            
-#            if nkey > 1:
-#                status[key]="granularity_staged"
-#            elif nkey == 1:
-#                status[key]="coverage_staged"
-#            else:
-#                if len([item for item in self.graph if item[0] == self.img[key][0]]) == 0:
-#                    status[key]="invalid"
-#                else: status[key]="valid"
-#                
-#        print(status) 
+#                # Initialize a dict for status and temp_status to temporary save the status of each label of an image 
+#                #status = defaultdict(list)
+#                temp_status = defaultdict(list)
+#
+#            
+#                # Is there any label for the image?
+#                if self.extract[key] == []:
+#                    self.extract_status[key] = "valid"
+#                    
+#                else:
+#                    # Is the label of the image a node of the graph?
+#                    for node in self.extract[key]:
+#                        
+#                        if node not in [item[0] for item in self.graph]:
+#                            temp_status[key].append("invalid")
+#                            
+#                        else:
+#                            # Does the node have new siblings since the extract was added?
+#                            if node in self.sib.keys():
+#                                new_nb_sib = self.count_sib(node) 
+#                                
+#                                if new_nb_sib > self.sib[node]:
+#                                    temp_status[key].append("coverage_staged")
+#                                    
+#                                # Does the node have any child node? 
+#                                else:
+#                                    if node in [item[1] for item in self.graph]:
+#                                        temp_status[key].append("granularity_staged")
+#            
+#                                    else: 
+#                                        temp_status[key].append("valid")
+#                    
+#                       
+#                                            
+#                        # Returns one status for each image
+#                        if "invalid" in temp_status[key]:
+#                            self.extract_status[key] = "invalid"
+#                        elif "coverage_staged" in temp_status[key]:
+#                            self.extract_status[key] = "coverage_staged"
+#                        elif "granularity_staged" in temp_status[key]:
+#                            self.extract_status[key] = "granularity_staged"
+#                        else: self.extract_status[key]= "valid"
+#    
 #        
         
-       
-     
-#    def get_extract_status(self):
-#        status = defaultdict(list)
-#        temp_status = defaultdict(list)
-#        for key in self.img:
-#            for i in range(0,len(self.img[key])-1):
-#                nkey = len([item for item in self.graph if item[1] == self.img[key][i]])
-#                
-#                if nkey > 1:
-#                    try: temp_status[key].append("granularity_staged")
-#                    except KeyError:
-#                        temp_status[key]=("granularity_staged")
-#                elif nkey == 1:
-#                    try: temp_status[key].append("coverage_staged")
-#                    except KeyError:
-#                        temp_status[key]="coverage_staged"
-#                else:
-#                    if len([item for item in self.graph if item[0] == self.img[key][i]]) == 0:
-#                        try: temp_status[key].append("invalid")
-#                        except KeyError:
-#                            temp_status[key]="invalid"
-#                    else: 
-#                        try: temp_status[key].append("valid")
-#                        except KeyError:
-#                            temp_status[key]="valid"
-#                        
-#            print(temp_status)        
-#            if "invalid" in temp_status[key]:
-#                status[key] = "invalid"
-#            elif "coverage_staged" in temp_status[key]:
-#                status[key] = "coverage_staged"
-#            elif "granularity_staged" in temp_status[key]:
-#                status[key] = "granularity_staged"
-#            else: status[key]= "valid"
-#            
-#            
-#        print(status) 
-                
         
-    
-#    def get_extract_status(self):
-#        status = defaultdict(list)
-#        temp_status = defaultdict(list)
-#        for key in self.img:
-#            for i in range(0,len(self.img[key])):
-#                nkey = len([item for item in self.graph if item[1] == self.img[key][i]])
-#                
-#                if nkey > 1:
-#                    temp_status[key].append("granularity_staged")
-#                elif nkey == 1:
-#                    temp_status[key].append("coverage_staged")
-#    
-#                else:
-#                    if len([item for item in self.graph if item[0] == self.img[key][i]]) == 0:
-#                        temp_status[key].append("invalid")
-#                    else: 
-#                        temp_status[key].append("valid")
-#                print(i)
-#                print(temp_status)
-#            
-#            if "invalid" in temp_status[key]:
-#                status[key] = "invalid"
-#            elif "coverage_staged" in temp_status[key]:
-#                status[key] = "coverage_staged"
-#            elif "granularity_staged" in temp_status[key]:
-#                status[key] = "granularity_staged"
-#            else: status[key]= "valid"
-#            
-#            
-#        print(status) 
-        
-        
-#    def get_extract_status(self):
-#        status = defaultdict(list)
-#        temp_status = defaultdict(list)
-#        for key in self.img:
-#            for i in range(0,len(self.img[key])):            
-#                if len([item for item in self.graph if item[1] == self.img[key][i]]) >= 1:
-#                    temp_status[key].append("granularity_staged")
-#                else:
-#                    if len([item for item in self.graph if item[0] == self.img[key][i]]) == 0:
-#                        temp_status[key].append("invalid")
-#                    else: 
-#                        if [item[1] for item in self.graph if item[0] == self.img[key][i]][0] in [item[1] for item in self.last_added if item[0]!=self.img[key][i]]:
-#                            temp_status[key].append("coverage_staged")
-#                        else:
-#                            temp_status[key].append("valid")
-#            
-#            if "invalid" in temp_status[key]:
-#                status[key] = "invalid"
-#            elif "coverage_staged" in temp_status[key]:
-#                status[key] = "coverage_staged"
-#            elif "granularity_staged" in temp_status[key]:
-#                status[key] = "granularity_staged"
-#            else: status[key]= "valid"
-#            
-#            
-#        print(status) 
-
-        
-     
-        
+           
     def get_extract_status(self):
+        """ 
+        Return the status of 
+        
+        Parameters:
+        extract : dictionnay key=image, value=node
+        
+        Returns:
+        """
+        # Initialize a dict for status and temp_status to temporary save the status of each label of an image 
         status = defaultdict(list)
         temp_status = defaultdict(list)
-        for key in self.img:
-            if self.img[key] == []:
+        
+        # For each image of the img dict, for each label, verify the status.
+        for key in self.extract:
+            
+            # Is there any label for the image?
+            if self.extract[key] == []:
                 status[key] = "valid"
+                
             else:
-                for node in self.img[key]:
+                # Is the label of the image a node of the graph?
+                for node in self.extract[key]:
+                    
                     if node not in [item[0] for item in self.graph]:
                         temp_status[key].append("invalid")
+                        
                     else:
-                        if node in self.bro.keys():
-                            new_nb_bro = self.count_bro(node) 
-                            if new_nb_bro > self.bro[node]:
-                                temp_status[key].append("coverage_staged")
+                        # Does the node have new siblings since the extract was added?
+
+                        new_nb_sib = self.count_sib(node) 
+                            
+                        if new_nb_sib > self.sib[str(key+','+node)]:
+                            temp_status[key].append("coverage_staged")
+                                
+                            # Does the node have any child node? 
+                        else:
+                            if node in [item[1] for item in self.graph]:
+                                temp_status[key].append("granularity_staged")
         
-                            else:
-                                if node in [item[1] for item in self.graph]:
-                                    temp_status[key].append("granularity_staged")
-        
-                                else: 
-                                    temp_status[key].append("valid")
+                            else: 
+                                temp_status[key].append("valid")
                 
                    
                                         
-                   # print(key,node)
-                    #print(temp_status[key])
-                    
+                    # Returns one status for each image
                     if "invalid" in temp_status[key]:
                         status[key] = "invalid"
                     elif "coverage_staged" in temp_status[key]:
@@ -232,38 +222,14 @@ class Database(object):
                         status[key] = "granularity_staged"
                     else: status[key]= "valid"
             
-        #self.bro.update([{item[0],self.count_bro(item[0])} for item in self.graph[1:]])
-        for item in self.graph[1:]:
-            self.bro[item[0]] = self.count_bro(item[0])
-           
-                    
-        print(status)            
-        return(status)           
+        #Update 
+        #for item in self.graph[1:]:
+        #    self.bro[item[0]] = self.count_bro(item[0])
+        #status = dict(zip(self.status_extract.keys(),[self.status_extract.values()[key][1] for key in self.status_extract.keys()]))
+        print(status)
+        
+        return(status)
+
+      
                     
             
-            
-#            if img[key] not in self.graph 
-#            for i in range(0,len(self.img[key])):            
-#                if len([item for item in self.graph if item[1] == self.img[key][i]]) >= 1:
-#                    temp_status[key].append("granularity_staged")
-#                else:
-#                    if len([item for item in self.graph if item[0] == self.img[key][i]]) == 0:
-#                        temp_status[key].append("invalid")
-#                    else: 
-#                        if [item[1] for item in self.graph if item[0] == self.img[key][i]][0] in [item[1] for item in self.last_added if item[0]!=self.img[key][i]]:
-#                            temp_status[key].append("coverage_staged")
-#                        else:
-#                            temp_status[key].append("valid")
-#            
-#            if "invalid" in temp_status[key]:
-#                status[key] = "invalid"
-#            elif "coverage_staged" in temp_status[key]:
-#                status[key] = "coverage_staged"
-#            elif "granularity_staged" in temp_status[key]:
-#                status[key] = "granularity_staged"
-#            else: status[key]= "valid"
-#            
-#            
-#        print(status) 
-
-
